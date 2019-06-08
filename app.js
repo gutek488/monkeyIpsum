610f7107e1aa27f7fae5c0ff805bfe9311568db4
gutek488 <42544936+gutek488@users.noreply.github.com>
2019-06-08 13:08:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+
+
+
+let monkeyText = document.querySelector(".monkeyText");
+let longest = document.querySelector(".longest");
+let wordsLength = document.querySelector(".wordsLength");
+let playStop = document.querySelector(".button");
+let playing = false;
+let foundDiv = document.querySelector(".found");
+let hideList = document.querySelector(".hideList");
+let mainInterval;
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let str = 0;
+let literature = "";
+let found = [];
+
+playStop.addEventListener("click", ()=>{
+        if (!playing) { 
+                mainInterval = setInterval(()=>{
+                        playStop.classList.add("stop");
+                        playing = true;
+                        literature += alphabet[Math.floor(Math.random() * 24)];
+                        searchWord();
+                        monkeyText.innerHTML+=literature[literature.length-1];
+                        if (found.length !== 0 ) {
+                                longest.innerText = "Longest word found: " + found.sort((a,b)=>b.length-a.length)[0]
+
+                        }
+                        //colorWords();
+                        wordsLength.innerText = "Number of words found: " + found.length; 
+                        },50)
+                } else  {
+                        clearInterval(mainInterval);
+                        playing = false;     
+                        playStop.classList.remove("stop");                   
+                }
+        
+
+
+
+})
+
+hideList.addEventListener("click", ()=>{
+        foundDiv.classList.toggle("found--active");
+        hideList.classList.toggle("hideList--rotate")
+
+
+})
+
+
+
+let searchWord = () => {
+    str++;
+    if (str === 6) {
+        for (let z of list) {
+                    if (literature.includes(z) && found.includes(z)===false) {
+                                found.push(z);
+                                let newWord = document.createElement(`a`);
+                                newWord.href = `https://en.oxforddictionaries.com/definition/us/${z}`;
+                                newWord.innerText = z;
+                                newWord.target ="_blank";
+                                newWord.classList.add("foundWord");
+                                foundDiv.appendChild(newWord);
+                                found.sort((a,b)=>b.length-a.length);
+
+                                
+
+                        }
+                        
+
+                }
+                str = 0;        
+    }
+
+   
+    // let re = new RegExp('^' + str);
+    // filteredList = filteredList.filter(function(val) {
+    //     return re.test(val);
+    // });
+    
+    // for (let x of filteredList) {
+    //     if (x === str) {
+    //         found.push(str);
+    //     }
+    // }    
+    // if (filteredList.length === 0) {
+    //     for (let z of list) {
+    //         if (str.includes(z) && z !== str) {
+    //             console.log(z, str);
+                
+    //             found.push(z);
+    //             console.log(found)
+    //         }
+    //     }
+        
+    //     str="";
+    //     filteredList=list;
+    // }    
+}
+
+// let colorWords = () => {
+//     for (let x of found) {
+// longest.innerText = "Longest word found: " + found.sort((a,b)=>b.length-a.length)[0]
+
+//       let reg = new RegExp("(" + x + ")(?![^<]*>|[^<>]*</)", "g");
+//       monkeyText.innerHTML = monkeyText.innerHTML.replace(reg, `<u>${x}</u>`);
+
+//         }
+//     }
+})  
+
